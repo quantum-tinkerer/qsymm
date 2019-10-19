ba6494746898e5fb5d193e564295e64f8445eb60
Joseph Weston <joseph@weston.cloud>
2019-10-19 19:27:52 +0200

    make default sphinx role API documentation links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# text in backticks should be treated as references to the API documentation
+default_role = "autolink"
 
 # -- Options for HTML output -------------------------------------------------
 
