bf42a6a00a2d45e7e770130aba8d6828e8947b69
Daniel Varjas <dvarjas@gmail.com>
2024-10-29 21:54:05 +0100

    add more tests for little group representations

--- qsymm/tests/test_groups.py
+++ qsymm/tests/test_groups.py
@@ -83,8 +83,58 @@ def test_pointgroup():
     pg.fix_U_phases()
     assert pg.consistent_U
     # Phase fixing can multiply the irrep by some 1D rep, result is not always the same
-    assert allclose(sum(pg.decompose_U_rep), 1)
+    assert allclose(sum(pg.decompose_U_rep[4:10]), 1)
 
     g = cubic(tr=False, ph=False, generators=True, spin=1/2, double_group=True)
     g = [PointGroupElement(h.R, U=np.kron(np.eye(2), h.U), RSU2=h.RSU2) for h in g]
     pg = PointGroup(g)
+    assert allclose(pg.decompose_U_rep, [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0])
+
+def test_little_group():
+    # SG198
+    C2z = rotation(1/2, [0, 0, 1], double_group=True)
+    C2z = SpaceGroupElement(C2z, t=[1/2, 0, 1/2], periods=np.eye(3))
+    C3 = rotation(1/3, [1, 1, 1], double_group=True)
+    C3 = SpaceGroupElement(C3, t=[0, 0, 0], periods=np.eye(3))
+
+    SG = SpaceGroup([C2z, C3])
+    assert len(SG.elements) == 24
+
+    # Gamma point
+    k = [0, 0, 0]
+    LG = SG.little_group(k)
+    assert len(LG.elements) == 24
+    irrep_dims = [1, 1, 1, 2, 2, 2, 3]
+    assert allclose(LG.character_table[:, 0], irrep_dims)
+    C2_column = [1, 1, 1, 0, 0, 0, -1]
+    assert allclose(LG.character_table[:, LG.class_by_element[LittleGroupElement(C2z, k)]], C2_column)
+    irreps = LG.irreps()
+    assert allclose([i.U_shape[0] for i in irreps], irrep_dims)
+    assert allclose([i.reality() for i in irreps], [0, 1, 0, 0, 0, -1, 1])
+
+    # R point
+    k = [1/2, 1/2, 1/2]
+    LG = SG.little_group(k)
+    assert len(LG.elements) == 24
+    irrep_dims = [1, 1, 1, 2, 2, 2, 3]
+    assert allclose(LG.character_table[:, 0], irrep_dims)
+    C2_column = [-1, -1, -1, 0, 0, 0, 1]
+    assert allclose(LG.character_table[:, LG.class_by_element[LittleGroupElement(C2z, k)]], C2_column)
+    irreps = LG.irreps()
+    assert allclose([i.U_shape[0] for i in irreps], irrep_dims)
+    assert allclose([i.reality() for i in irreps], [0, 1, 0, 0, 0, -1, 1])
+
+    # M point
+    k = [0, 1/2, 1/2]
+    LG = SG.little_group(k)
+    assert len(LG.elements) == 8
+    irrep_dims = [1, 1, 1, 1, 2]
+    ### TODO: Double-check this, seems inconsistent or different convention than Bilbao
+    # Could be just a different gauge for the projective representation
+    # assert allclose(LG.character_table[:, 0], irrep_dims)
+    # C2_column = [1j, -1j, 1j, -1j, 0]
+    # assert allclose(LG.character_table[:, LG.class_by_element[LittleGroupElement(C2z, k)]], C2_column)
+    # irreps = LG.irreps()
+    # assert allclose([i.U_shape[0] for i in irreps], irrep_dims)
+    # Reality also doesn't match, that should be gauge invariant
+    # assert allclose([i.reality() for i in irreps], [1, 1, 1, 1, -1])
