2b0a30c8547c70bb71ca9dc3573c5c6a49ae8889
Daniel Varjas <dvarjas@gmail.com>
2024-11-21 13:48:24 +0100

    add test for k-point where antiunitary is C2T

--- qsymm/tests/test_groups.py
+++ qsymm/tests/test_groups.py
@@ -293,7 +293,8 @@ def test_little_group_198(k, irrep_dims, C2_column, reality):
         ([0, 0, 0], [4, 2]),
         ([1/2, 1/2, 1/2], [2, 2, 2, 6]),
         ([1/2, 1/2, 0], [4]),
-        ([1/2, 0, 0], [2, 2])
+        ([1/2, 0, 0], [2, 2]),
+        ([0.1, 0.2, 1/2], [2])
     ]
 )
 def test_little_group_198_TR(k, irrep_dims):
